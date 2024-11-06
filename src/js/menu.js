bf248e0f9aa7a4bcc9803ef4672e422c40e1af81
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-11-06 22:27:42 +0200

    Clean up styles

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,11 @@
 const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMenuOpenButton = document.querySelector('.mobile-menu-open-btn'); // Assuming you have a button to open the menu
+const mobileMenuOpenButton = document.querySelector('.mobile-menu-open-btn');
 const mobileMenuCloseButton = document.querySelector('.mobile-menu-close-btn');
 
-// Open the mobile menu
 mobileMenuOpenButton.addEventListener('click', () => {
   mobileMenu.classList.add('is-open');
 });
 
-// Close the mobile menu
 mobileMenuCloseButton.addEventListener('click', () => {
   mobileMenu.classList.remove('is-open');
 });
