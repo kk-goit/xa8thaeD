f45d2db50e401662447cede6bef50f8883d6a920
Vitalii Kostenko <apelsin@i.ua>
2024-11-07 12:16:49 +0100

    section-title-and-menu finalized

--- src/js/exercises-menu.js
+++ src/js/exercises-menu.js
@@ -1,6 +1,10 @@
 import { renderGroupListByFilter } from './group-list';
 
-export const execisesMenuButtonHandler = (element, buttons) => {
+const buttons = Array.from(document.querySelectorAll('.exercises-menu-button'));
+
+const execisesMenuButtonHandler = (element) => {
+    const sectionTitle = document.querySelector(".section-title");
+    sectionTitle.innerHTML = "Exercises";
     buttons.forEach(el => {
         el.classList.remove("active");
     });
@@ -8,3 +12,7 @@ export const execisesMenuButtonHandler = (element, buttons) => {
 
     renderGroupListByFilter({ filter: element.textContent })
 }
+
+buttons.forEach(el =>
+    el.addEventListener('click', () => execisesMenuButtonHandler(el))
+);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import './js/menu.js';
 import './js/header.js';
 import './js/group-list.js';
-import { execisesMenuButtonHandler } from './js/exercises-menu';
-
-const buttons = Array.from(document.querySelectorAll('.exercises-menu-button'));
-
-buttons.forEach(el =>
-    el.addEventListener('click', () => execisesMenuButtonHandler(el, buttons))
-);
+import './js/exercises-menu.js';
