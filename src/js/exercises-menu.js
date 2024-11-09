12ae04d516cd5cb42001310eb2fbbd91b35f42a3
Vitalii Kostenko <apelsin@i.ua>
2024-11-09 08:50:29 +0100

    layout ready

--- src/js/exercises-menu.js
+++ src/js/exercises-menu.js
@@ -4,11 +4,14 @@ const buttons = Array.from(document.querySelectorAll('.exercises-menu-button'));
 
 const execisesMenuButtonHandler = (element) => {
     const sectionTitle = document.querySelector(".section-title");
+    const exercisesForm = document.querySelector('.exercises-form');
+
     sectionTitle.innerHTML = "Exercises";
     buttons.forEach(el => {
         el.classList.remove("active");
     });
     element.classList.add("active");
+    exercisesForm.classList.add("visually-hidden");
 
     renderGroupListByFilter({ filter: element.textContent })
 }
