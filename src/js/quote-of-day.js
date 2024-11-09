912cee64273bb8cdfe90afc62f7ba4f9ee7a9812
Vitalii Kostenko <apelsin@i.ua>
2024-11-09 12:45:17 +0100

    bugs fixed

--- src/js/quote-of-day.js
+++ src/js/quote-of-day.js
@@ -24,4 +24,8 @@ if(quoteInStorage && quoteInStorage.date === today) {
                 console.log(response);
             }
         })
+        .catch(() => {
+            quote.innerHTML = "A lot of times I find that people who are blessed with the most talent don't ever develop that attitude, and the ones who aren't blessed in that way are the most competitive and have the biggest heart.";
+            author.innerHTML = "Tom Brady";
+        })
 }
