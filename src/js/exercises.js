d02f3eec754cc4b1fff51249165a6c01717cbf28
Kostiantyn Kolomiichuk <kkolomiychuk@cleack.com>
2024-11-09 23:33:39 +0200

    added time for burned calaries in exercice's card

--- src/js/exercises.js
+++ src/js/exercises.js
@@ -174,7 +174,7 @@ function renderUserListExercises(element, listExercises) {
             .join(' ')}</p>
     </div>
     <div class="details">
-        <p>Burned calories: <span>${exercise.burnedCalories}</span></p>
+        <p>Burned calories: <span>${exercise.burnedCalories} / ${exercise.time} min</span></p>
         <p>Body part: <span>${exercise.bodyPart}</span></p>
         <p>Target: <span>${exercise.target}</span></p>
     </div>
