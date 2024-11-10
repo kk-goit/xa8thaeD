1c69277cfbf220a6ee53183ff71338ec74b9489e
Andrii Korchevnyi <andrey.korchevnyi@pragmaticdlt.com>
2024-11-10 19:39:25 +0200

    timer merged

--- src/js/timer-info.js
+++ src/js/timer-info.js
@@ -1,27 +1,30 @@
 checkTimer();
 
 function setTime() {
-    const timeLine = document.querySelector('.all-time');
-    const calories = document.querySelector('.calories');
+  const timeLine = document.querySelector('.all-time');
+  const calories = document.querySelector('.calories');
 
-    const time = Math.floor((localStorage.getItem('timer') || 0) / 60);
-    const burntCalories = localStorage.getItem('burntCalories') || 0;
+  const time = Math.floor((localStorage.getItem('timer') || 0) / 60);
+  const burntCalories = localStorage.getItem('burntCalories') || 0;
+  console.log('==== time, burntCalories ==> ', time, burntCalories);
 
-    timeLine.innerText = `${time} min`;
-    calories.innerText = Math.floor(Number(burntCalories) / 1000).toString();
-    console.log('==== calories.innerText ==> ', calories.innerText);
-    if (time > 110) timeLine.style.color = 'green';
+  timeLine.innerText = `${time || 0} min`;
+  calories.innerText = Math.floor(Number(burntCalories) / 1000 || 0).toString();
+
+  if (time > 110) timeLine.style.color = 'green';
 }
+
+setTime();
 
 export default setTime;
 
 
 function checkTimer() {
-    const lastUpdate = localStorage.getItem('lastUpdate');
-    const today = new Date().toDateString();
-    if (lastUpdate === today) return;
+  const lastUpdate = localStorage.getItem('lastUpdate');
+  const today = new Date().toDateString();
+  if (lastUpdate === today) return;
 
-    localStorage.setItem('lastUpdate', today);
-    localStorage.setItem('timer', 0);
-    localStorage.setItem('burntCalories', 0);
+  localStorage.setItem('lastUpdate', today);
+  localStorage.setItem('timer', 0);
+  localStorage.setItem('burntCalories', 0);
 }
